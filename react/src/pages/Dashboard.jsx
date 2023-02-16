2eef588eaa7f7504c01a4076d11cb6090ca4e066
Ibrahimhussien227 <ibrahimhussien227@gmail.com>
2023-02-16 12:56:01 +0300

    Form CSS

--- react/src/pages/Dashboard.jsx
+++ react/src/pages/Dashboard.jsx
@@ -9,7 +9,28 @@ const Dashboard = () => {
     return <Navigate to="/admin/users" />;
   }
 
-  return <div>Dashboard</div>;
+  return (
+    <div className="feedback-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={() => {}}>
+          <h1 className="title">Feedback</h1>
+          {/* {errors && (
+            <div className="alert">
+              {Object.keys(errors).map((key) => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          )} */}
+          <input type="text" placeholder="Subject" />
+          <textarea placeholder="Enter your feedback" />
+          <input type="file" placeholder="Password" />
+          <button className="btn btn-block" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
