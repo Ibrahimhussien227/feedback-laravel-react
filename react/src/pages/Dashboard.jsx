ec7e723787c931bf45177afd71c664fa151588d6
Ibrahimhussien227 <ibrahimhussien227@gmail.com>
2023-02-17 16:06:16 +0300

    dashboard page (form feedback)

--- react/src/pages/Dashboard.jsx
+++ react/src/pages/Dashboard.jsx
@@ -1,30 +1,83 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Navigate } from "react-router-dom";
+
 import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../axios-client";
+import { Loader } from "../components";
 
 const Dashboard = () => {
-  const { user, token, setUser, setToken } = useStateContext();
+  const { user } = useStateContext();
+  const [loading, setLoading] = useState(false);
+  const [request, setRequest] = useState({
+    subject: "",
+    body: "",
+    file: "",
+  });
+  const [errors, setErrors] = useState(null);
+  const ref = useRef();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axiosClient
+      .post("/feedback/create", request, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then(() => {
+        setRequest({ subject: "", body: "", file: "" });
+        ref.current.value = "";
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        const { response } = err;
+        if (response && response.status === 422) {
+          setErrors(response.data.errors);
+        }
+      });
+  };
 
   if (user.role === 1) {
-    return <Navigate to="/admin/users" />;
+    return <Navigate to="/admin/feedback" />;
   }
+
+  if (loading) return <Loader />;
 
   return (
     <div className="feedback-form animated fadeInDown">
       <div className="form">
-        <form onSubmit={() => {}}>
+        <form onSubmit={onSubmit}>
           <h1 className="title">Feedback</h1>
-          {/* {errors && (
+          {errors && (
             <div className="alert">
               {Object.keys(errors).map((key) => (
                 <p key={key}>{errors[key][0]}</p>
               ))}
             </div>
-          )} */}
-          <input type="text" placeholder="Subject" />
-          <textarea placeholder="Enter your feedback" />
-          <input type="file" placeholder="Password" />
-          <button className="btn btn-block" type="submit">
+          )}
+          <input
+            value={request.subject}
+            onChange={(e) =>
+              setRequest({ ...request, subject: e.target.value })
+            }
+            type="text"
+            placeholder="Subject"
+          />
+          <textarea
+            value={request.body}
+            onChange={(e) => setRequest({ ...request, body: e.target.value })}
+            placeholder="Enter your feedback"
+          />
+          <input
+            ref={ref}
+            onChange={(e) =>
+              setRequest({ ...request, file: e.target.files[0] })
+            }
+            type="file"
+          />
+          <button className="btn btn-form" type="submit">
             Submit
           </button>
         </form>
